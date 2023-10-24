651d1b44f0ce547528c07c706666b7d8c2337dff
mabyoungg <mabyoungg@gmail.com>
2023-10-24 13:03:14 +0900

    feat: 1단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        wiseSaying app = new wiseSaying();
+
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+
+            Scanner sc = new Scanner(System.in);
+            String status = sc.next();
+            if (status.equals("종료")) {
+                app.exit();
+
+            }
+        }
+
+    }
+}
+
+class wiseSaying {
+    void exit() {
+        System.exit(0);
     }
 }
