7fc8f2bbe5545dd05c8359ab5bc516bbbb11f735
mabyoungg <mabyoungg@gmail.com>
2023-10-24 14:10:02 +0900

    feat: 5단계 목록

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +15,8 @@ public class Main {
             String status = sc.next();
             if (status.equals("등록")) {
                 app.create();
+            } else if (status.equals("목록")) {
+                app.list();
             } else if (status.equals("종료")) {
                 app.exit();
             }
@@ -24,19 +26,56 @@ public class Main {
 }
 
 class wiseSaying {
-    int count = 0;
+    int count = 1;
+    ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
     void create() {
+        Map<String, Object> map = new HashMap<>();
+
         Scanner sc = new Scanner(System.in);
+
+        map.put("번호",count);
+
         System.out.print("명언 : ");
-        String status = sc.next();
+        String word = sc.next();
+        map.put("명언",word);
+
         System.out.print("작가 : ");
         String author = sc.next();
+        map.put("작가", author);
+
+        list.add(map);
+
+        System.out.println(count + "번 명언이 등록되었습니다.");
 
         count++;
 
-        System.out.println(count + "번 명언이 등록되었습니다.");
     }
+
+    void list() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("--------------------");
+
+        for (int i = 0; i <= list.size()-1; i++) {
+            System.out.print(list.get(i).get("번호"));
+            System.out.print(" / ");
+            System.out.print(list.get(i).get("작가"));
+            System.out.print(" / ");
+            System.out.println(list.get(i).get("명언"));
+        }
+
+
+//        Iterator<String> keys = map.keySet().iterator();
+//
+//        while (keys.hasNext()) {
+//            String key = keys.next();
+//
+//            System.out.println(count + " / " + key + " / " + map.get(key));
+//
+//            count++;
+//        }
+    }
+
     void exit() {
         System.exit(0);
     }
