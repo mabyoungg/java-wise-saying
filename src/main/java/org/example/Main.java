ddb698f3d2af32fbfa9662cf1817b9c825dda607
mabyoungg <mabyoungg@gmail.com>
2023-10-24 15:08:31 +0900

    feat: 8단계 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,8 @@ public class Main {
                 app.list();
             } else if (status.startsWith("삭제?id=")) {
                 app.delete(status);
+            } else if (status.startsWith("수정?id=")) {
+                app.update(status);
             } else if (status.equals("종료")) {
                 app.exit();
             }
@@ -81,7 +83,7 @@ class wiseSaying {
     void delete(String status) {
         int index = status.indexOf("=");
         int findNum =  Integer.parseInt(status.substring(index+1));
-        int deleteNum = 0;
+        int deleteNum = -1;
 
         for (int i = 0; i <= list.size()-1; i++) {
             if (list.get(i).get("번호").equals(findNum)) {
@@ -89,18 +91,54 @@ class wiseSaying {
 
                 list.remove(deleteNum);
                 System.out.println(findNum + "번 명언이 삭제되었습니다.");
-            } else {
-                System.out.println(findNum + "번 명언은 존재하지 않습니다.");
             }
-
         }
 
+        if (deleteNum == -1) {
+            System.out.println(findNum + "번 명언은 존재하지 않습니다.");
+        }
 
     }
+
+    void update(String status) {
+        int index = status.indexOf("=");
+        int findNum =  Integer.parseInt(status.substring(index+1));
+        int updateNum = -1;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i <= list.size()-1; i++) {
+            if (list.get(i).get("번호").equals(findNum)) {
+                updateNum = i;
+                Map<String, Object> map = new HashMap<>();
+
+                System.out.println("명언(기존) : "+list.get(updateNum).get("명언"));
+                System.out.print("명언 : ");
+                String word = sc.next();
+                map.put("명언",word);
+
+                System.out.println("작가(기존) : "+list.get(updateNum).get("작가"));
+                System.out.print("작가 : ");
+                String author = sc.next();
+                map.put("작가",author);
+
+                map.put("번호",findNum);
+
+                list.set(updateNum,map);
+
+                System.out.println(findNum + "번 명언이 수정되었습니다.");
+            }
+        }
+         if (updateNum == -1)  {
+            System.out.println(findNum + "번 명언은 존재하지 않습니다.");
+        }
+    }
+
 
     void exit() {
         System.exit(0);
     }
+
 
 
 }
