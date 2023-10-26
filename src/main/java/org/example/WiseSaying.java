6addbce6064de0ee30ca912c3bab26cc1b809254
mabyoungg <mabyoungg@gmail.com>
2023-10-26 18:04:45 +0900

    refactor: 등록 객체화

---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class WiseSaying {
+    int count;
+    String word;
+    String author;
+
+    WiseSaying(int count, String word, String author) {
+        this.count = count;
+        this.word = word;
+        this.author = author;
+    }
+
+}
